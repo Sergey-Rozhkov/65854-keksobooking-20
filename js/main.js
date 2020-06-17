454cb77b1f756a64429e47fe3d32143d0b902dc9
viktoria.lazor@gmail.com <viktoria.lazor@gmail.com>
2020-06-17 09:42:53 +0300

    Fixes for Personal project: more details (part 1)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var ADVERT_NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8'];
-var ADVERT_PRICE = ['300', '400', '500', '600', '700', '800', '900', '1000'];
-var ADVERT_TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var ADVERT_CHECKIN_CHECKOUT = ['12:00', '13:00', '14:00'];
+var ADVERT_PRICES = ['300', '400', '500', '600', '700', '800', '900', '1000'];
+var ADVERT_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var ADVERT_CHECKINS_CHECKOUTS = ['12:00', '13:00', '14:00'];
 var ADVERT_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var ADVERT_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var ADVERT_AMOUNTS = 8;
 
 var getRandomNumberFromRange = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -19,47 +20,41 @@ var getRandomItemArray = function (arr) {
   return result;
 };
 
-var getRandomIndexArray = function (arr) {
-  var min = 0;
-  var max = arr.length - 1;
-  var result = Math.round(Math.random() * (max - min) + min);
-
-  return arr[result];
-};
-
 var getArrayRandomLength = function (arr) {
-  var newArray = [];
-  var number = getRandomNumberFromRange(1, arr.length - 1);
-  for (var i = 0; i <= number; i++) {
-    newArray.push(arr[i]);
-  }
+  var newArray = arr.slice(getRandomNumberFromRange(-(arr.length - 1), arr.length - 1));
 
   return newArray;
 };
 
 var adverts = [];
 
-for (var i = 0; i < 8; i++) {
+for (var i = 0; i < ADVERT_AMOUNTS; i++) {
   var number = getRandomItemArray(ADVERT_NUMBERS);
   var advertLocationX = getRandomNumberFromRange(0, 1160);
   var advertLocationY = getRandomNumberFromRange(130, 630);
   var advertRooms = getRandomNumberFromRange(1, 10);
 
   var advert = {
-    avatar: 'img/avatars/user0' + number + '.png',
-    title: 'Заголовок предложения ' + number,
-    address: advertLocationX + ', ' + advertLocationY,
-    price: getRandomIndexArray(ADVERT_PRICE),
-    type: getRandomIndexArray(ADVERT_TYPE),
-    rooms: advertRooms,
-    guests: advertRooms * 2,
-    checkin: getRandomIndexArray(ADVERT_CHECKIN_CHECKOUT),
-    checkout: getRandomIndexArray(ADVERT_CHECKIN_CHECKOUT),
-    features: getArrayRandomLength(ADVERT_FEATURES),
-    description: 'Описание сдающегося объекта ' + number,
-    photos: getArrayRandomLength(ADVERT_PHOTOS),
-    locationX: advertLocationX + 'px',
-    locationY: advertLocationY + 'px',
+    author: {
+      avatar: 'img/avatars/user0' + number + '.png',
+    },
+    offer: {
+      title: 'Заголовок предложения ' + number,
+      address: advertLocationX + ', ' + advertLocationY,
+      price: ADVERT_PRICES[getRandomNumberFromRange(0, ADVERT_PRICES.length - 1)],
+      type: ADVERT_TYPES[getRandomNumberFromRange(0, ADVERT_TYPES.length - 1)],
+      rooms: advertRooms,
+      guests: advertRooms * 2,
+      checkin: ADVERT_CHECKINS_CHECKOUTS[getRandomNumberFromRange(0, ADVERT_CHECKINS_CHECKOUTS.length - 1)],
+      checkout: ADVERT_CHECKINS_CHECKOUTS[getRandomNumberFromRange(0, ADVERT_CHECKINS_CHECKOUTS.length - 1)],
+      features: getArrayRandomLength(ADVERT_FEATURES),
+      description: 'Описание сдающегося объекта ' + number,
+      photos: getArrayRandomLength(ADVERT_PHOTOS),
+    },
+    location: {
+      x: advertLocationX + 'px',
+      y: advertLocationY + 'px',
+    }
   };
 
   adverts.push(advert);
@@ -68,26 +63,27 @@ for (var i = 0; i < 8; i++) {
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
-var mapPins = document.querySelector('.map__pins');
+var mapPinsElement = document.querySelector('.map__pins');
 
-var pinTemplate = document.querySelector('#pin')
+var pinTemplateElement = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
 
 var renderPin = function (item) {
-  var pin = pinTemplate.cloneNode(true);
+  var pin = pinTemplateElement.cloneNode(true);
+  var imgElement = pin.querySelector('img');
 
-  pin.querySelector('img').setAttribute('src', item.avatar);
-  pin.querySelector('img').setAttribute('alt', item.title);
-  pin.style.left = item.locationX;
-  pin.style.top = item.locationY;
+  imgElement.setAttribute('src', item.author.avatar);
+  imgElement.setAttribute('alt', item.offer.title);
+  pin.style.left = item.location.x;
+  pin.style.top = item.location.y;
 
   return pin;
 };
 
 var fragment = document.createDocumentFragment();
-for (var j = 0; j < 8; j++) {
+for (var j = 0; j < ADVERT_AMOUNTS; j++) {
   fragment.appendChild(renderPin(adverts[j]));
 }
 
-mapPins.appendChild(fragment);
+mapPinsElement.appendChild(fragment);
